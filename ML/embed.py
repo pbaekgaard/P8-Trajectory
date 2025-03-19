8d86d87d6bd1623e4630287be405e516994ef99c
Rasmus <rasmu@LAPTOP-08K6AG11>
2025-03-19 12:13:26 +0100

    Delete files not used anymore

--- ML/embed.py
+++
@@ -1,7 +0,0 @@
-import pandas as pd
-
-def embed_df(df: pd.DataFrame):
-    pass
-
-if "__main__" == __name__:
-    print("embed kælling, ")
--- tools/scripts/_load_data.py
+++ tools/scripts/_load_data.py
@@ -8,9 +8,19 @@ import pandas as pd
 # Suppress FutureWarnings
 warnings.simplefilter(action='ignore', category=FutureWarning)
 DATA_DIR = os.path.abspath(os.path.join(os.path.dirname(__file__), "../../data/taxi_log_2008_by_id"))
+COMPRESSED_DATA_DIR = os.path.abspath(os.path.join(os.path.dirname(__file__), "../../data/compressed_data"))
 
 def main() -> pd.DataFrame :
     files = glob.glob(os.path.join(DATA_DIR, "*.txt"))
+
+    headers = ["trajectory_id", "timestamp", "longitude", "latitude"]
+    dfs = [pd.read_csv(file, names=headers, delimiter=",").dropna(inplace=False) for file in files]
+
+    df = pd.concat(dfs, ignore_index=True)
+    return df
+
+def load_compressed_data():
+    files = glob.glob(os.path.join(COMPRESSED_DATA_DIR, "*.txt"))
 
     headers = ["trajectory_id", "timestamp", "longitude", "latitude"]
     dfs = [pd.read_csv(file, names=headers, delimiter=",").dropna(inplace=False) for file in files]
