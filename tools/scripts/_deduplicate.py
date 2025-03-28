1c264364c60cd78cc9d3ce2138c4d79f42c49105
Rasmus <rasmu@LAPTOP-08K6AG11>
2025-03-28 15:32:52 +0100

    Count query finished and removal of illegal points in preprocessing implemented

--- tools/scripts/_deduplicate.py
+++ tools/scripts/_deduplicate.py
@@ -3,5 +3,6 @@ import pandas as pd
 
 def main(data: pd.DataFrame) -> pd.DataFrame:
     data.drop_duplicates(subset=['trajectory_id', 'timestamp'], keep='first', inplace=True)
+    data = data[~(data.duplicated(subset=['trajectory_id', 'longitude', 'latitude'], keep='first') & data.duplicated(subset=['trajectory_id', 'longitude', 'latitude'], keep='last'))]
     return data
 
