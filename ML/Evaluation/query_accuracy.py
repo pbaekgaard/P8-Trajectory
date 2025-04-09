029b13cf3bc443f9fea7a8eec8fbc3d8a9b05d50
Rasmus <rasmu@LAPTOP-08K6AG11>
2025-04-09 15:40:26 +0200

    Skeleton for calculation of query accuracy

--- ML/Evaluation/query_accuracy.py
+++ ML/Evaluation/query_accuracy.py
@@ -1,12 +1,20 @@
-import pandas as pd
-
-from tools.scripts._preprocess import main as _load_data
-from tools.scripts._load_data import load_compressed_data as _load_compressed_data
-
 from ML.Evaluation._file_access_helper_functions import save_to_file, load_data_from_file
 
 
 def query_accuracy_evaluation():
     # Load compressed results and original results and calculate accuracy
-    pass
+    # compressed_results = load_data_from_file("compressed_query_results")
+    original_results = load_data_from_file({
+        "filename": "test-original_query_results"
+    })
+    queries = load_data_from_file({
+        "filename": "queries_for_evaluation",
+    })
+
+
+    # print(compressed_results)
+    print(original_results)
+
+
+query_accuracy_evaluation()
 
