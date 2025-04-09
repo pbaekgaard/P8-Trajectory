294edfcba95262535bd442a21603c132af7e8122
Aasmundur <aakt21@student.aau.dk>
2025-04-09 14:23:54 +0200

    made osc

--- OSTC/src/trajectory.cpp
+++ OSTC/src/trajectory.cpp
@@ -12,18 +12,18 @@
 #include <cstdint>
 #endif
 
-bool SamplePoint::operator==(const SamplePoint& other) const
-{
+bool SamplePoint::operator==(const SamplePoint &other) const {
     return longitude == other.longitude && latitude == other.latitude && timestamp == other.timestamp;
 }
 
-Trajectory::Trajectory(const uint32_t id, const std::vector<SamplePoint>& points): id(id), points(points) {}
-Trajectory::Trajectory(const uint32_t id, const std::vector<SamplePoint>& points, const short start_index,
-                       const short end_index): id(id), points(points), start_index(start_index), end_index(end_index)
-{}
+Trajectory::Trajectory(const uint32_t id, const std::vector<SamplePoint> &points): id(id), points(points) {
+}
 
-Trajectory Trajectory::operator()(const short start, const short end)
-{
+Trajectory::Trajectory(const uint32_t id, const std::vector<SamplePoint> &points, const short start_index,
+                       const short end_index): id(id), points(points), start_index(start_index), end_index(end_index) {
+}
+
+Trajectory Trajectory::operator()(const short start, const short end) {
     if (end + 1 > points.size()) {
         return Trajectory(id, std::vector<SamplePoint>(points.begin() + start, points.begin() + end), start, end);
     }
@@ -31,49 +31,45 @@ Trajectory Trajectory::operator()(const short start, const short end)
     return Trajectory(id, std::vector<SamplePoint>(points.begin() + start, points.begin() + end + 1), start, end);
 }
 
-Trajectory Trajectory::operator+(Trajectory other)
-{
+Trajectory Trajectory::operator+(Trajectory other) {
     auto mergedPoints = points;
-    std::copy(other.points.begin()+1, other.points.end(), std::back_inserter(mergedPoints));
+    std::copy(other.points.begin() + 1, other.points.end(), std::back_inserter(mergedPoints));
 
     return Trajectory(id, mergedPoints, start_index, other.end_index);
 }
 
-bool Trajectory::operator==(const Trajectory& other) const
-{
+bool Trajectory::operator==(const Trajectory &other) const {
     return (id == other.id && start_index == other.start_index && end_index == other.end_index);
 }
 
-bool ReferenceTrajectory::operator==(const ReferenceTrajectory& other) const
-{
+bool ReferenceTrajectory::operator==(const ReferenceTrajectory &other) const {
     return (id == other.id && start_index == other.start_index && end_index == other.end_index);
 }
 
-std::size_t std::hash<Trajectory>::operator()(const Trajectory& t) const noexcept
-{
+std::size_t std::hash<Trajectory>::operator()(const Trajectory &t) const noexcept {
     return ((hash<uint32_t>()(t.id) ^ (hash<short>()(t.start_index) << 2) ^ (hash<short>()(t.end_index) >> 1) ^
              (hash<uint32_t>()(t.points.size()) << 1)) >>
             1);
 }
 
-ReferenceTrajectory::ReferenceTrajectory(const uint32_t id, const short start_index, const short end_index):
-    id(id), start_index(start_index), end_index(end_index)
-{}
+ReferenceTrajectory::ReferenceTrajectory(const uint32_t id, const short start_index,
+                                         const short end_index): id(id), start_index(start_index),
+                                                                 end_index(end_index) {
+}
 
-ReferenceTrajectory::ReferenceTrajectory(const Trajectory& t):
-    id(t.id), start_index(t.start_index), end_index(t.end_index)
-{}
+ReferenceTrajectory::ReferenceTrajectory(const Trajectory &t): id(t.id), start_index(t.start_index),
+                                                               end_index(t.end_index) {
+}
 
-std::unordered_map<Trajectory, std::vector<ReferenceTrajectory>> Trajectory::MRTSearch(std::vector<Trajectory>& RefSet,
-                                                                                       const double epsilon)
-{
-    std::unordered_map<Trajectory, std::vector<Trajectory>> M;
+std::unordered_map<Trajectory, std::vector<ReferenceTrajectory> > Trajectory::MRTSearch(std::vector<Trajectory> &RefSet,
+    const double epsilon) {
+    std::unordered_map<Trajectory, std::vector<Trajectory> > M;
 
-    for (auto i = 0; i < points.size()-1; i++) {
+    for (auto i = 0; i < points.size() - 1; i++) {
         auto j = i + 1;
         auto current_sub_traj = (*this)(i, j);
-        for (auto& ref_trajectory : RefSet) {
-            for (auto k = 0; k < ref_trajectory.points.size()-1; k++) {
+        for (auto &ref_trajectory: RefSet) {
+            for (auto k = 0; k < ref_trajectory.points.size() - 1; k++) {
                 auto l = k + 1;
                 auto ref_sub_traj = ref_trajectory(k, l);
 
@@ -85,6 +81,7 @@ std::unordered_map<Trajectory, std::vector<ReferenceTrajectory>> Trajectory::MRT
     }
 
     for (auto n = 3; n < points.size(); n++) {
+        bool found = false;
         for (auto k = 0; k + n - 1 < points.size(); k++) {
             auto lengthNSubtrajectory = (*this)(k, k + n - 1);
 
@@ -95,81 +92,102 @@ std::unordered_map<Trajectory, std::vector<ReferenceTrajectory>> Trajectory::MRT
             auto T_a_vector = T_a_entry != M.end() ? T_a_entry->second : std::vector<Trajectory>();
             auto T_b_vector = T_b_entry != M.end() ? T_b_entry->second : std::vector<Trajectory>();
 
-            for (const auto& T_a : T_a_vector) {
+            for (auto &T_a: T_a_vector) {
                 if (maxDTW(lengthNSubtrajectory, T_a) <= epsilon) {
+                    found = true;
                     M[lengthNSubtrajectory].push_back(T_a);
                 }
-            }
-
-            for (const auto& T_b : T_b_vector) {
-                if (maxDTW(lengthNSubtrajectory, T_b) <= epsilon) {
-                    M[lengthNSubtrajectory].push_back(T_b);
-                }
-            }
-
-            for (auto& T_a : T_a_vector) {
-                for (const auto& T_b : T_b_vector) {
+                for (const auto &T_b: T_b_vector) {
+                    if (maxDTW(lengthNSubtrajectory, T_b) <= epsilon) {
+                        found = true;
+                        M[lengthNSubtrajectory].push_back(T_b);
+                    }
                     if (T_a.id == T_b.id && T_a.end_index == T_b.start_index) {
+                        found = true;
                         M[lengthNSubtrajectory].push_back(T_a + T_b);
                     }
                 }
             }
         }
+        if (!found) { break; }
     }
 
-    std::unordered_map<Trajectory, std::vector<ReferenceTrajectory>> M1;
-    for (const auto& pair : M) {
+    std::unordered_map<Trajectory, std::vector<ReferenceTrajectory> > M1;
+    for (const auto &pair: M) {
         std::vector<ReferenceTrajectory> refTrajectories;
         refTrajectories.reserve(pair.second.size());
 
-        for (const auto& ref_trajectory : pair.second) {
+        for (const auto &ref_trajectory: pair.second) {
             refTrajectories.emplace_back(ref_trajectory);
         }
 
         M1[pair.first] = refTrajectories;
     }
 
-    std::vector<std::pair<Trajectory, std::vector<Trajectory>>> vec(M.begin(), M.end());
+    std::vector<std::pair<Trajectory, std::vector<Trajectory> > > vec(M.begin(), M.end());
 
     // Sort by the Trajectory's id (or whatever other logic you'd like)
-    std::sort(vec.begin(), vec.end(), [](const auto& a, const auto& b) {
-        return a.first.start_index < b.first.start_index;  // or any other sort criteria
+    std::sort(vec.begin(), vec.end(), [](const auto &a, const auto &b) {
+        return a.first.start_index < b.first.start_index; // or any other sort criteria
     });
-    auto found = M1.find((*this)(2,9));
+    auto found = M1.find((*this)(2, 9));
     if (found != M1.end() && !found->second.empty()) {
-        for (const auto f : found->second) {
-            std::cout<< "id: " << f.id << " Start: " <<f.start_index << " end: " << f.end_index<< std::endl;
+        for (const auto f: found->second) {
+            std::cout << "id: " << f.id << " Start: " << f.start_index << " end: " << f.end_index << std::endl;
         }
     }
     return M1;
 }
 
-std::vector<ReferenceTrajectory> Trajectory::OSTC(std::unordered_map<Trajectory, std::vector<ReferenceTrajectory>> M)
-{
+std::vector<ReferenceTrajectory> Trajectory::OSTC(std::unordered_map<Trajectory, std::vector<ReferenceTrajectory> > M) {
     std::vector<int> Ft(points.size(), 0);
-    std::vector<int> pre(points.size(), -1);
-    std::vector<ReferenceTrajectory> T_prime {};
-    for (short i=0; i<points.size(); i++) {
+    std::vector<short> pre(points.size(), 0);
+    std::vector<ReferenceTrajectory> T_prime{};
+
+    for (short i = 1; i < points.size(); i++) {
         //i og j skal måske være 1 før det virker, pga Ft[0] = 0 i pseudo koden.
         //Men så skal trajectories 0-indexeres, og så skal fx mrt search ændres også
-        int min = 8*points.size(); //obs på om denne narrowing giver fejl
-        for (short j=0; j<i; j++) { // maybe stop condition giver fejl sowwy
-            auto key = (*this)(j, i);
+        int min = 8 * points.size(); //obs på om denne narrowing giver fejl
+        for (short j = 1; j <= i; j++) {
+            // maybe stop condition giver fejl sowwy
+            auto key = (*this)(j-1, i-1);
             auto it = M.find(key);
 
-            if (it != M.end() && !it -> second.empty()) {
-                if (j>0 && Ft[j-1] + 8 < min) {
-                    min = Ft[j-1] + 8;
+            if (it != M.end()) {
+                const int cost = Ft[j - 1] + 8;
+                if (cost < min) {
+                    min = cost;
                     pre[i] = j - 1;
                 }
             }
         }
         Ft[i] = min;
     }
-    auto i = points.size();
-    while (i>0 && i<= points.size()) {
+    short i = points.size()-1;
+    while (i > 0 && i <= points.size()) {
+        if (pre[i] == i - 1) {
+            T_prime.push_back((*this)(i,i));
+            i--;
+        } else {
+            auto it = M.find((*this)(pre[i]+1,i));
+            if (it != M.end()) {
+                auto reference_trajectories = it->second;
 
+                auto longest_index = 0;
+                auto longest_trajectory = 0;
+
+                for (auto reference_traj : reference_trajectories) {
+                    auto traj_length = reference_traj.end_index - reference_traj.start_index;
+                    if (traj_length > longest_trajectory) {
+                        longest_trajectory = traj_length;
+                        longest_index = reference_traj.id;
+                    }
+                }
+
+                T_prime.push_back(reference_trajectories[0]);
+            }
+            i = pre[i];
+        }
     }
     return T_prime;
 }
-
