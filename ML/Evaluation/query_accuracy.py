84317c79f2a640435f5ee0b57c7b5d0537a4a40c
Rasmus <rasmu@LAPTOP-08K6AG11>
2025-04-10 14:10:57 +0200

    Meh

--- ML/Evaluation/query_accuracy.py
+++ ML/Evaluation/query_accuracy.py
@@ -4,16 +4,20 @@ from ML.Evaluation._file_access_helper_functions import save_to_file, load_data_
 def query_accuracy_evaluation():
     # Load compressed results and original results and calculate accuracy
     # compressed_results = load_data_from_file("compressed_query_results")
-    original_results = load_data_from_file({
+    test_original_results = load_data_from_file({
         "filename": "test-original_query_results"
     })
+    original_results = load_data_from_file({
+            "filename": "original_query_results"
+    })
+
     queries = load_data_from_file({
         "filename": "queries_for_evaluation",
     })
 
 
     # print(compressed_results)
-    print(original_results)
+    print(test_original_results)
 
 
 query_accuracy_evaluation()
