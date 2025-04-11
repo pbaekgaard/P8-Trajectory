c2a5fd5a27d3abc1a5fd6a2510131bea441b6115
runeb <runeberhardt@gmail.com>
2025-04-11 09:52:36 +0200

    query accuracy init

--- ML/Evaluation/main.py
+++ ML/Evaluation/main.py
@@ -9,7 +9,8 @@ from tools.scripts._preprocess import main as _load_data
 from tools.scripts._load_data import load_compressed_data as _load_compressed_data
 from ML.Evaluation.query_creation import create_queries, dummy_create_queries
 from ML.Evaluation._file_access_helper_functions import load_data_from_file
-from ML.Evaluation.querying import query_original_dataset
+from ML.Evaluation.querying import query_original_dataset, query_compressed_dataset
+from ML.Evaluation.query_accuracy import query_accuracy_evaluation
 
 
 data = [
@@ -37,6 +38,47 @@ data = [
         ]
 
 
+def mock_compressed_data():
+    reference_set = [
+        [0, "2008-02-02 15:36:08", 116.51172, 39.92123, {1: "2008-02-02 14:00:00"}],  # Trajectory 1
+        [0, "2008-02-02 15:40:10", 116.51222, 39.92173, {1: "2008-02-02 14:15:00", 2: "2008-02-02 16:12:00"}],
+        [0, "2008-02-02 16:00:00", 116.51372, 39.92323, None],
+
+        [4, "2008-02-02 17:10:00", 116.57000, 39.97000, {5: "2008-02-02 18:00:00"}],  # Trajectory 5
+        [4, "2008-02-02 17:15:00", 116.58000, 39.98000, {5: "2008-02-02 18:02:30"}]
+    ]
+
+    reference_set_df = pd.DataFrame(reference_set, columns=["trajectory_id", "timestamp", "longitude", "latitude",
+                                                            "timestamp_corrected"])
+
+    compressed_data = {
+        0: [(0, 0, 2)],
+        1: [(0, 0, 1)],
+        2: [(0, 0, 1)],
+        3: [(3, 0, 2)],
+        4: [(4, 0, 1)],
+        5: [(4, 0, 1), (5, 0, 1)],
+    }
+
+    original_data_trimmed = [
+        [3, "2008-02-02 13:30:00", 116.50050, 39.91050],  # Trajectory 4
+        [3, "2008-02-02 13:45:00", 116.52050, 39.93050],
+        [3, "2008-02-02 14:00:00", 116.54050, 39.95050],
+
+        [5, "2008-02-02 20:05:00", 116.60000, 39.99200],
+        [5, "2008-02-02 20:10:00", 116.61000, 39.99300]
+    ]
+
+    original_df_trimmed = pd.DataFrame(original_data_trimmed,
+                                       columns=["trajectory_id", "timestamp", "longitude", "latitude"])
+
+    merged_df = pd.concat([reference_set_df, original_df_trimmed], ignore_index=True).sort_values(
+        ["trajectory_id", "timestamp"])
+
+    merged_df["timestamp_corrected"] = merged_df["timestamp_corrected"].fillna(False)
+
+    return compressed_data, merged_df
+
 if __name__ == '__main__':
     parser = argparse.ArgumentParser()
 
@@ -51,10 +93,22 @@ if __name__ == '__main__':
     })
     #queries = dummy_create_queries()
     if not os.path.exists(os.path.join(os.path.abspath(__file__), "..", "files", "original_query_results.pkl")) or args.force:
-        dataset, _ = _load_data()
+        dataset = _load_data()
         #dataset = pd.DataFrame(data, columns=["trajectory_id", "timestamp", "longitude", "latitude"])
         query_original_dataset(dataset, queries)
     if not os.path.exists(os.path.join(os.path.abspath(__file__), "..", "files", "compressed_query_results.pkl")) or args.force:
-        pass
+        compressed_dataset, merged_df = mock_compressed_data()
+        # compressed_dataset, merged_df = _load_compressed_data()
+        query_compressed_dataset(compressed_dataset, merged_df, queries)
+
+
+    original_results = load_data_from_file({
+        "filename": "original_query_results",
+    })
+    compressed_results = load_data_from_file({
+        "filename": "compressed_query_results",
+    })
+
+    accuracy = query_accuracy_evaluation(original_results, compressed_results)
 
     print("Querying done")
--- ML/Evaluation/query_accuracy.py
+++ ML/Evaluation/query_accuracy.py
@@ -1,24 +1,54 @@
+import pandas as pd
+
+from tools.scripts._preprocess import main as _load_data
+from tools.scripts._load_data import load_compressed_data as _load_compressed_data
+
 from ML.Evaluation._file_access_helper_functions import save_to_file, load_data_from_file
 
 
-def query_accuracy_evaluation():
-    # Load compressed results and original results and calculate accuracy
-    # compressed_results = load_data_from_file("compressed_query_results")
-    test_original_results = load_data_from_file({
-        "filename": "test-original_query_results"
-    })
-    original_results = load_data_from_file({
-            "filename": "original_query_results"
-    })
+def query_accuracy_evaluation(y_true, y_pred):
+    accuracy_results = []
+    # WHERE
+    accuracy_results.append(where_query_accuracy_evaluation(y_true[0], y_pred[0]))
+    # DISTANCE
+    accuracy_results.append(distance_query_accuracy_evaluation(y_true[1], y_pred[1]))
+    # WHEN
+    accuracy_results.append(when_query_accuracy_evaluation(y_true[2], y_pred[2]))
+    # HOW LONG
+    accuracy_results.append(how_long_query_accuracy_evaluation(y_true[3], y_pred[3]))
+    # COUNT
+    accuracy_results.append(count_query_accuracy_evaluation(y_true[4], y_pred[4]))
+    # KNN
+    accuracy_results.append(knn_query_accuracy_evaluation(y_true[5], y_pred[5]))
+    # WINDOW
+    accuracy_results.append(window_query_accuracy_evaluation(y_true[6], y_pred[6]))
 
-    queries = load_data_from_file({
-        "filename": "queries_for_evaluation",
-    })
+    return sum(accuracy_results) / len(accuracy_results)
+
+def where_query_accuracy_evaluation(y_true, y_pred):
+    return 0.5
+
+def distance_query_accuracy_evaluation(y_true, y_pred):
+    return 0.5
 
 
-    # print(compressed_results)
-    print(test_original_results)
+def when_query_accuracy_evaluation(y_true, y_pred):
+    return 0.5
 
 
-query_accuracy_evaluation()
+def how_long_query_accuracy_evaluation(y_true, y_pred):
+    return 0.5
 
+def count_query_accuracy_evaluation(y_true, y_pred):
+    results = []
+    for i in range(0, len(y_true)):
+        abs(y_true[i] - y_pred[i]) / max(abs(y_true[i]), abs(y_pred[i]))
+    return 0.5
+
+
+def knn_query_accuracy_evaluation(y_true, y_pred):
+    return 0.5
+
+
+def window_query_accuracy_evaluation(y_true, y_pred):
+    return 0.5
