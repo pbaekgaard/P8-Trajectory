4e489179411bf58af2c0f004edecaa74e03f5141
Xaniah <rasmus21@student.aau.dk>
2025-05-02 15:06:09 +0200

    Created functionality to compute the deviation between two reference sets

--- Evaluation/reference_set_comparison.py
+++ Evaluation/reference_set_comparison.py
@@ -1,5 +1,6 @@
 import sys
 import os
+from collections import Counter
 
 sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), "../src/components")))
 
@@ -9,16 +10,30 @@ from _file_access_helper_functions import get_best_params
 from tools.scripts._preprocess import main as _load_data
 from tools.scripts._delimit_trajectory_length import main as delimit_trajectory_length
 
-def compute_similarity_of_reference_sets(reference_set_one, reference_set_two):
-    # This only counts if the numbers are equal, but does not consider if the same clusters were formed but with different centroids.
-    # I don't know if it actually makes sense if it does that tho.
-    zipped_list = list(zip(reference_set_one, reference_set_two))
+def compute_deviation_of_likeliness_of_traj_in_ref_set(likeliness_ref_set_one: dict, likeliness_ref_set_two: dict):
+    # Compute the deviation of the likeliness that a trajectory appears a reference set
+    total_deviation = 0
 
+    for key, value in likeliness_ref_set_one.items():
+        if key not in likeliness_ref_set_two:
+            total_deviation += value
+        else:
+            total_deviation += abs(value - likeliness_ref_set_two[key])
 
+    for key, value in likeliness_ref_set_two.items():
+        if key not in likeliness_ref_set_one:
+            total_deviation += value
 
-    return sum(1 for a, b in zipped_list if a == b) / len(zipped_list)
+    normalized_deviation = total_deviation / len(set(likeliness_ref_set_one.keys()).union(set(likeliness_ref_set_two.keys())))
+    return normalized_deviation
 
+def similarity_comparison(full_representative_indices, limited_representative_indices):
+    matches = len(set(full_representative_indices).intersection(limited_representative_indices))
 
+    return matches / max(len(full_representative_indices), len(limited_representative_indices))
+
+def likeliness_comparison(full_representative_indices, limited_representative_indices):
+    return full_representative_indices, limited_representative_indices
 
 def reference_set_comparison():
     print('Reference Set Comparison')
@@ -38,31 +53,54 @@ def reference_set_comparison():
     print(len(full_dataset))
     print(len(limited_dataset))
 
-    clustering_method, clustering_param, batch_size, d_model, num_heads, clustering_metric, num_layers = get_best_params()
-    full_df, _, full_reference_set, full_representative_indices, full_trajectory_representations = generate_reference_set(
-        df=full_dataset, clustering_method=clustering_method, clustering_param=clustering_param,
-        batch_size=batch_size, d_model=d_model, num_heads=num_heads, clustering_metric=clustering_metric,
-        num_layers=num_layers
-    )
+    iterations = 10
+    similarities = []
+    likeliness_ref_set_one = {}
+    likeliness_ref_set_two = {}
 
-    limited_df, _, limited_reference_set, limited_representative_indices, limited_trajectory_representations = generate_reference_set(
-        df=limited_dataset, clustering_method=clustering_method, clustering_param=clustering_param,
-        batch_size=batch_size, d_model=d_model, num_heads=num_heads, clustering_metric=clustering_metric,
-        num_layers=num_layers
-    )
+    for i in range(iterations):
+        clustering_method, clustering_param, batch_size, d_model, num_heads, clustering_metric, num_layers = get_best_params()
+        full_df, _, full_reference_set, full_representative_indices, full_trajectory_representations = generate_reference_set(
+            df=full_dataset, clustering_method=clustering_method, clustering_param=clustering_param,
+            batch_size=batch_size, d_model=d_model, num_heads=num_heads, clustering_metric=clustering_metric,
+            num_layers=num_layers
+        )
 
-    visualize_in_PCA(full_df, full_trajectory_representations, full_representative_indices, full_reference_set, clustering_method, "Full")
-    visualize_in_PCA(limited_df, limited_trajectory_representations, limited_representative_indices, limited_reference_set, clustering_method, "Limited")
+        limited_df, _, limited_reference_set, limited_representative_indices, limited_trajectory_representations = generate_reference_set(
+            df=limited_dataset, clustering_method=clustering_method, clustering_param=clustering_param,
+            batch_size=batch_size, d_model=d_model, num_heads=num_heads, clustering_metric=clustering_metric,
+            num_layers=num_layers
+        )
 
-    full_reference_set = [x + 1 for x in full_reference_set]
-    limited_reference_set = [x + 1 for x in limited_reference_set]
+        # visualize_in_PCA(full_df, full_trajectory_representations, full_representative_indices, full_reference_set,
+        #                  clustering_method, "Full")
+        # visualize_in_PCA(limited_df, limited_trajectory_representations, limited_representative_indices,
+        #                  limited_reference_set, clustering_method, "Limited")
 
-    print("Full reference set ", full_reference_set)
-    print("Limited reference set ", limited_reference_set)
+        similarities.append(similarity_comparison(full_representative_indices, limited_representative_indices))
+        for value in full_representative_indices:
+            if value in likeliness_ref_set_one:
+                likeliness_ref_set_one[value].append(1)  # 1 represents that it was in the reference set here
+            else:
+                likeliness_ref_set_one[value] = [1]  # 1 represents that it was in the reference set here
 
-    similarity_score = compute_similarity_of_reference_sets(full_reference_set, limited_reference_set)
+        for value in limited_representative_indices:
+            if value in likeliness_ref_set_two:
+                likeliness_ref_set_two[value].append(1) # 1 represents that it was in the reference set here
+            else:
+                likeliness_ref_set_two[value] = [1] # 1 represents that it was in the reference set here
 
-    print(similarity_score)
+    similarity = sum(similarities) / iterations
+
+    likeliness_ref_set_one = {key: sum(value) / iterations for key, value in likeliness_ref_set_one.items()}
+    likeliness_ref_set_two = {key: sum(value) / iterations for key, value in likeliness_ref_set_two.items()}
+    deviation = compute_deviation_of_likeliness_of_traj_in_ref_set(likeliness_ref_set_one, likeliness_ref_set_two)
+
+    print(similarity)
+    print(likeliness_ref_set_one)
+    print(likeliness_ref_set_two)
+    print(deviation)
+
 
 if __name__ == '__main__':
     reference_set_comparison()
