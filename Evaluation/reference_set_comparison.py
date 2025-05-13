ede3c6ba5c5ca595c4167c84314d48a153aecf3c
Xaniah <rasmus21@student.aau.dk>
2025-05-13 11:32:02 +0200

    Added the ability to easily run the comparison multiple times with different lengths of trajectories as well as different ref set sizes

--- Evaluation/reference_set_comparison.py
+++ Evaluation/reference_set_comparison.py
@@ -2,6 +2,8 @@ import sys
 import os
 from collections import Counter
 
+from shapely.measurement import length
+
 sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), "../src/components")))
 
 from ML.reference_set_construction import generate_reference_set, visualize_in_PCA
@@ -35,7 +37,7 @@ def similarity_comparison(full_representative_indices, limited_representative_in
 def likeliness_comparison(full_representative_indices, limited_representative_indices):
     return full_representative_indices, limited_representative_indices
 
-def reference_set_comparison():
+def reference_set_comparison(length, ref_set_size):
     print('Reference Set Comparison')
     full_dataset = _load_data(
         [   'deduplication',
@@ -43,15 +45,10 @@ def reference_set_comparison():
             'limit_samplerate',
             'remove_illegal',
             'convert_timestamp_to_unix',
-            'take_10_trajectories', # Remove if you want every trajectory
         ]
     )
-
-    length = 10
+    # full_dataset = full_dataset[full_dataset["trajectory_id"].isin(full_dataset["trajectory_id"].drop_duplicates().iloc[:500])]
     limited_dataset = delimit_trajectory_length(full_dataset, length)
-
-    print(len(full_dataset))
-    print(len(limited_dataset))
 
     iterations = 10
     similarities = []
@@ -60,6 +57,9 @@ def reference_set_comparison():
 
     for i in range(iterations):
         clustering_method, clustering_param, batch_size, d_model, num_heads, clustering_metric, num_layers = get_best_params()
+        if clustering_method.name == "KMEDOIDS":
+            clustering_param = ref_set_size
+
         full_df, _, full_reference_set, full_representative_indices, full_trajectory_representations = generate_reference_set(
             df=full_dataset, clustering_method=clustering_method, clustering_param=clustering_param,
             batch_size=batch_size, d_model=d_model, num_heads=num_heads, clustering_metric=clustering_metric,
@@ -96,11 +96,15 @@ def reference_set_comparison():
     likeliness_ref_set_two = {key: sum(value) / iterations for key, value in likeliness_ref_set_two.items()}
     deviation = compute_deviation_of_likeliness_of_traj_in_ref_set(likeliness_ref_set_one, likeliness_ref_set_two)
 
-    print(similarity)
-    print(likeliness_ref_set_one)
-    print(likeliness_ref_set_two)
-    print(deviation)
+    return similarity, deviation
 
 
 if __name__ == '__main__':
-    reference_set_comparison()
+    lengths = [10, 50, 100]
+    ref_set_sizes = [5, 25, 50]
+    lengths_and_ref_set_size = [(length, ref_set_size) for length in lengths for ref_set_size in ref_set_sizes]
+    for length, ref_set_size in lengths_and_ref_set_size:
+        similarity, deviation = reference_set_comparison(length, ref_set_size)
+        print(similarity)
+        print(deviation)
+
